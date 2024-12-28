6c7dd0edd83a54337e5a85a55274da036f86b28c
leonorgonz <lellogonz@gmail.com>
2024-12-28 20:42:10 +0100

    escena linterna

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-AOS.init();
+document.addEventListener("DOMContentLoaded", () => {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector("[data-scroll-container]"),  // Contenedor principal
+        smooth: true,
+        multiplier: 1,
+        class: 'is-revealed',
+    });
+});
 
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector("#direction"),
-//     smooth: true,
-// });
+$(function() {
+    // Hacer que el elemento con id "draggable" sea arrastrable
+    $("#draggable").draggable();
+
+    // Hacer que el elemento con id "droppable" sea una zona de "drop"
+    $("#droppable").droppable({
+        drop: function(event, ui) {
+            $(this).addClass("ui-state-highlight").find("p").html("");
+
+            // Hacer que la imagen se muestre poco a poco
+            $("#droppedImage").fadeIn(2000); // 2 segundos para hacerla visible
+
+            // Hacer que la linterna desaparezca
+            $("#draggable").fadeOut(2000); // 1 segundo para ocultar la linterna
+        }
+    });
+});
